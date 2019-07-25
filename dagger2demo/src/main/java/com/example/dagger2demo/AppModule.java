17d015a9948e8e544dd843c3b731fb20239b6c5a
bsp <bsp0911932@163.com>
2019-07-25 09:29:56 +0800

    some log

--- dagger2demo/src/main/java/com/example/dagger2demo/AppModule.java
+++ dagger2demo/src/main/java/com/example/dagger2demo/AppModule.java
@@ -3,16 +3,20 @@ package com.example.dagger2demo;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.example.dagger2demo.annotations.ApplicationContext;
 import com.example.uilib.Toaster;
 import com.example.uilib.impl.ToasterImpl;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
+    private static final String TAG = AppModule.class.getSimpleName();
     private final Application mApplication;
 
     public AppModule(Application app) {
@@ -32,11 +36,14 @@ public class AppModule {
 
     @Provides
     SharedPreferences provideSharedPrefs() {
+        Log.i(TAG, "provideSharedPrefs called");
         return mApplication.getSharedPreferences("default", Context.MODE_PRIVATE);
     }
 
     @Provides
+    @Singleton
     Toaster provideToaster(@ApplicationContext Context context) {
+        Log.i(TAG, "provideToaster called");
         return new ToasterImpl(context);
     }
 }
